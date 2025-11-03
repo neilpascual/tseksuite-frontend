220a69bbe3c66bc1477fedea548cb16d18e374f6
Anonyyymouse <ravalbert17@gmail.com>
2025-11-03 13:26:22 +0800

    Feat: Sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App.jsx";
+import AdminSideBar from "./components/admin/AdminSideBar.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <AdminSideBar />
+  </StrictMode>
+);
