135c945bea49745de3bb03c12648801e544c8f7b
Anonyyymouse <ravalbert17@gmail.com>
2025-11-04 15:25:41 +0800

    Fix: Sidebar Navigation and Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import AdminSideBar from './components/admin/AdminSideBar.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
