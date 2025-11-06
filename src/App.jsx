95e53d37465b97f9175e7fe9a948da30b6db733a
neilpascual <neilaronpascual.dev@gmail.com>
2025-11-06 14:39:21 +0800

    feat: minor changes in UI

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import TestBankPage from "./pages/admin/AssesmentsTab/TestBankPage";
 import ErrorMessage from "./pages/admin/ErrorMessage";
 import TestInstructions from "./pages/applicant/TestInstructions";
 import CompletedTestPage from "./pages/applicant/CompletedTestPage";
+import TestBuilderPage from "./pages/admin/AssesmentsTab/TestBuilderPage";
 
 function App() {
   return (
@@ -46,7 +47,7 @@ function App() {
 
             {/* assessments submenu routes */}
             <Route path="assessments/test-bank" element={<TestBankPage />} />
-            <Route path="assessments/test-builder" element={<ComingSoon />} />
+            <Route path="assessments/test-builder" element={<TestBuilderPage />} />
             {/* <Route path="admin/applicants/results" element={<ResultsPage />} /> */}
           </Route>
         </Route>
--- src/pages/admin/AssesmentsTab/TestBuilderPage.jsx
+++ src/pages/admin/AssesmentsTab/TestBuilderPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function TestBuilderPage() {
   return (
-    <div>TestBuilderPage</div>
+    <div className="h-screen w-full px-3 sm:px-6 md:px-8 py-6">
+      <h1 className="text-[#2E99B0] text-md sm:text-md md:text-xl lg:text-2xl xl:text-3xl mb-6 font-['Poppins']">
+          Test Builder
+        </h1>
+    </div>
   )
 }
 
