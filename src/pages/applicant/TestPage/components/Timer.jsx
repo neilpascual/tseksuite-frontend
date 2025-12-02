42184c4ee45f4f8d000251aade8d9b91a0da449c
markabano <markabanomain@gmail.com>
2025-12-02 11:58:56 +0800

    fix: clock svg

--- src/pages/applicant/TestPage/components/Timer.jsx
+++ src/pages/applicant/TestPage/components/Timer.jsx
@@ -1,4 +1,4 @@
-import ClockIcon from "../../../../assets/clock.svg";
+import ClockIcon from "../../../../assets/Clock.svg";
 
 const Timer = ({ timeRemaining }) => (
   <div
@@ -6,9 +6,7 @@ const Timer = ({ timeRemaining }) => (
     style={{ boxShadow: "2px 2px 0px 0px rgba(0, 0, 0, 1)" }}
   >
     <img src={ClockIcon} className="w-5 h-5" alt="clock" />
-    <span className="font-semibold text-gray-900 text-lg">
-      {timeRemaining}
-    </span>
+    <span className="font-semibold text-gray-900 text-lg">{timeRemaining}</span>
   </div>
 );
 
