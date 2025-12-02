3a51deb15a71aa7f642799e2c7f06b17ce0fa5c9
markabano <markabanomain@gmail.com>
2025-12-02 11:25:39 +0800

    fix: try fix of tabletview

--- src/pages/admin/ApplicantsTab/TestPage/index.jsx
+++ src/pages/admin/ApplicantsTab/TestPage/index.jsx
@@ -14,7 +14,7 @@ import Pagination from "./components/Pagination";
 import LoadingState from "./components/LoadingState";
 import EmptyState from "./components/EmptyState";
 import MobileView from "./components/views/MobileView";
-import TabletView from "./components/views/Tabletview";
+import TabletView from "./components/views/TabletView";
 import DesktopView from "./components/views/DesktopView";
 
 function TestsPage() {
